dac78aa2c2a1ef66742a83d0cf73a51607d319cc
Moosees <linus.almgren@gmail.com>
2020-02-16 23:58:37 +0100

    Clean up code and add comments

--- maze/index.js
+++ maze/index.js
@@ -1,12 +1,4 @@
-const {
-  Engine,
-  Render,
-  Runner,
-  World,
-  Bodies,
-  Mouse,
-  MouseConstraint
-} = Matter;
+const { Engine, Render, Runner, World, Bodies } = Matter;
 
 // Grid size calculations
 const width = document.body.clientWidth;
@@ -30,10 +22,65 @@ const render = Render.create({
   }
 });
 
-Engine.run(engine);
-Render.run(render);
+// Setup initial wall grid
+const vericalWallLayout = Array(verticalCells).fill(
+  Array(horizontalCells - 1).fill(true)
+);
 
-// Outer walls
+const horizontalWallLayout = Array(verticalCells - 1).fill(
+  Array(horizontalCells).fill(true)
+);
+
+// Setup maze for traversal
+const maze = Array(verticalCells).fill(Array(horizontalCells).fill(false));
+const startingRoom = {
+  x: Math.floor(Math.random() * horizontalCells),
+  y: Math.floor(Math.random() * verticalCells)
+};
+const path = [startingRoom];
+
+// Maze helper functions
+const checkRoom = (x, y, maze) => {
+  return (
+    x >= 0 && x < horizontalCells && y >= 0 && y < verticalCells && !maze[y][x]
+  );
+};
+
+const findAdjacentRooms = (x, y, maze) => {
+  let adjacentRooms = [];
+  if (checkRoom(x, y - 1, maze)) {
+    adjacentRooms.push({ x, y: y - 1, direction: 'up' });
+  }
+  if (checkRoom(x + 1, y, maze)) {
+    adjacentRooms.push({ x: x + 1, y, direction: 'right' });
+  }
+  if (checkRoom(x, y + 1, maze)) {
+    adjacentRooms.push({ x, y: y + 1, direction: 'down' });
+  }
+  if (checkRoom(x - 1, y, maze)) {
+    adjacentRooms.push({ x: x - 1, y, direction: 'left' });
+  }
+  return adjacentRooms;
+};
+
+const buildMaze = (maze, path, verticalWalls, horizontalWalls) => {
+  while (path.length) {
+    const adjacentRooms = findAdjacentRooms(path[0].x, path[0].y, maze);
+    if (adjacentRooms.length) {
+      const direction = Math.floor(Math.random() * adjacentRooms.length);
+      const nextRoom = adjacentRooms[direction];
+      maze[nextRoom.y][nextRoom.x] = true;
+      path.push(nextRoom);
+    } else {
+      path.pop();
+    }
+  }
+};
+
+// Generate the maze
+buildMaze(maze, path, vericalWallLayout, horizontalWallLayout);
+
+// Styling for the walls
 const wallOptions = {
   isStatic: true,
   render: {
@@ -41,22 +88,17 @@ const wallOptions = {
   }
 };
 
-const walls = [
+// Setup outer walls
+const outerWalls = [
   Bodies.rectangle(width / 2, 0, width, 10, wallOptions),
   Bodies.rectangle(width / 2, height, width, 10, wallOptions),
   Bodies.rectangle(0, height / 2, 10, height, wallOptions),
   Bodies.rectangle(width, height / 2, 10, height, wallOptions)
 ];
-World.add(world, walls);
 
-// Vertical walls
-const showVerticalWalls = Array(verticalCells).fill(
-  Array(horizontalCells - 1).fill(true)
-);
-
+// Determine what walls to render
 const verticalWalls = [];
-
-showVerticalWalls.forEach((row, y) => {
+vericalWallLayout.forEach((row, y) => {
   row.forEach((wall, x) => {
     if (wall)
       verticalWalls.push(
@@ -71,16 +113,8 @@ showVerticalWalls.forEach((row, y) => {
   });
 });
 
-World.add(world, verticalWalls);
-
-// Horizontal walls
-const showHorizontalWalls = Array(verticalCells - 1).fill(
-  Array(horizontalCells).fill(true)
-);
-
 const horizontalWalls = [];
-
-showHorizontalWalls.forEach((row, y) => {
+horizontalWallLayout.forEach((row, y) => {
   row.forEach((wall, x) => {
     if (wall)
       horizontalWalls.push(
@@ -95,51 +129,9 @@ showHorizontalWalls.forEach((row, y) => {
   });
 });
 
+// Render the maze
+Engine.run(engine);
+Render.run(render);
+World.add(world, outerWalls);
+World.add(world, verticalWalls);
 World.add(world, horizontalWalls);
-
-// Build maze
-const maze = Array(verticalCells).fill(Array(horizontalCells).fill(false));
-const startingRoom = {
-  x: Math.floor(Math.random() * horizontalCells),
-  y: Math.floor(Math.random() * verticalCells)
-};
-const path = [startingRoom];
-
-const checkRoom = (x, y, maze) => {
-  return (
-    x >= 0 && x < horizontalCells && y >= 0 && y < verticalCells && !maze[y][x]
-  );
-};
-
-const findAdjacentRooms = (x, y, maze) => {
-  let adjacentRooms = [];
-  if (checkRoom(x, y - 1, maze)) {
-    adjacentRooms.push({ x, y: y - 1 });
-  }
-  if (checkRoom(x + 1, y, maze)) {
-    adjacentRooms.push({ x: x + 1, y });
-  }
-  if (checkRoom(x, y + 1, maze)) {
-    adjacentRooms.push({ x, y: y + 1 });
-  }
-  if (checkRoom(x - 1, y, maze)) {
-    adjacentRooms.push({ x: x - 1, y });
-  }
-  return adjacentRooms;
-};
-
-const buildMaze = (maze, path) => {
-  while (path.length) {
-    const adjacentRooms = findAdjacentRooms(path[0].x, path[0].y, maze);
-    if (adjacentRooms.length) {
-      const direction = Math.floor(Math.random() * adjacentRooms.length);
-      const nextRoom = adjacentRooms[direction];
-      maze[nextRoom.y][nextRoom.x] = 'banana';
-      path.push(nextRoom);
-    } else {
-      path.pop();
-    }
-  }
-};
-
-buildMaze(maze, path);
